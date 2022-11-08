9548eb41e43ba0d5bfca5c39284478d85300dd6b
LB3368 <latisha2801@gmail.com>
2022-11-08 15:47:13 -0500

    updated code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//This is where I declare/define my various fields(selectors) to make the pieces of my game
 const selectors = {
     boardContainer: document.querySelector('.board-container'),
     board: document.querySelector('.board'),
@@ -7,12 +8,13 @@ const selectors = {
     win: document.querySelector('.win')
 }
 
+// The state of the game will keep track of different values
 const state = {
-    gameStarted: false,
-    flippedCards: 0,
-    totalFlips: 0,
-    totalTime: 0,
-    loop: null
+    gameStarted: false, //checks if game has started
+    flippedCards: 0, // used to keep track of the number of flips
+    totalFlips: 0, // keep track of the total number of flips
+    totalTime: 0, // keeps track of the time since game started
+    loop: null // will update the timer every second
 }
 
 const shuffle = array => {
@@ -28,16 +30,24 @@ const shuffle = array => {
         // Math.floor and math.random allow a random element to be used from an array
         const original = clonedArray[index]
 
-        clonedArray[index] = clonedArray[randomIndex]
+        clonedArray[index] = clonedArray[randomIndex] // stores the original index value in a variable to prevent overwriting
         clonedArray[randomIndex] = original
     }
 
-    return clonedArray
+    return clonedArray // will return a descending 'for' loop created by a random index using Math.random and Math.floor
 }
 
+// Random arrays 
 const pickRandom = (array, items) => {
+    //pickRandom loops through the passed array and gets an item from it at a random position and returns the "randomPicks" at the end of the function passing it to the "shuffle" function
+    /**
+     * -- To shuffle an array a of n elements (indices 0..n-1):
+for i from n−1 downto 1 do
+   j ← random integer such that 0 ≤ j ≤ i
+   exchange a[j] and a[i]
+     */
     //allows the array to be iterated over and expanded in places where zero or more arguments or elements are expected
-    const clonedArray = [...array]
+    const clonedArray = [...array] 
     const randomPicks = []
 
     for (let index = 0; index < items; index++) {
@@ -50,16 +60,19 @@ const pickRandom = (array, items) => {
     return randomPicks
 }
 
+// create a function to generate the board 
 const generateGame = () => {
     const dimensions = selectors.board.getAttribute('data-dimension')
 
+    // passing an even number into the dimension using a remainder(modulo)
     if (dimensions % 2 !== 0) {
         throw new Error("The dimension of the board must be an even number.")
     }
 
+    // generate an array of random emojis
     const emojis = ['🥔', '🍒', '🥑', '🌽', '🥕', '🍇', '🍉', '🍌', '🥭', '🍍', '🎃', '💥', '🦇', '💀', '😱', '🕷️', '👻', '👺', '🎇', '✨']
-    const picks = pickRandom(emojis, (dimensions * dimensions) / 2) 
-    const items = shuffle([...picks, ...picks])
+    const picks = pickRandom(emojis, (dimensions * dimensions) / 2)  // picks half the items of dimensions
+    const items = shuffle([...picks, ...picks]) //making sure that get two pairs for each emoji for shuffling, order will be random each time the page is reloaded
     const cards = `
         <div class="board" style="grid-template-columns: repeat(${dimensions}, auto)">
             ${items.map(item => `
@@ -77,11 +90,12 @@ const generateGame = () => {
     selectors.board.replaceWith(parser.querySelector('.board'))
 }
 
+//Starting the Game
 const startGame = () => {
-    state.gameStarted = true
-    selectors.start.classList.add('disabled')
+    state.gameStarted = true // tells if the game is running
+    selectors.start.classList.add('disabled') // disables the "start" button to prevent starting game over and over after game has begun
 
-    state.loop = setInterval(() => {
+    state.loop = setInterval(() => { // runs every second to update with the total number of flips and elapsed time.
         state.totalTime++
 
         selectors.moves.innerText = `${state.totalFlips} moves`
@@ -89,22 +103,26 @@ const startGame = () => {
     }, 1000)
 }
 
-const flipBackCards = () => {
+// Flipping The Cards
+const backCardFlip = () => {
     document.querySelectorAll('.card:not(.matched)').forEach(card => {
         card.classList.remove('flipped')
     })
 
-    state.flippedCards = 0
+    state.flippedCards = 0 // all the card that are not matched, removes .flipped class  sets the flippedCards back to 0 to continue flipping two cards at a time
 }
 
-const flipCard = card => {
+// Updating the "state" of the game, everytime a card is flipped, increment both the cardsFlipped and totalFlips
+const cardFlip = card => {
     state.flippedCards++
     state.totalFlips++
 
+    // check to see if game has already started; if not, call the startGame function
     if (!state.gameStarted) {
         startGame()
     }
 
+    // check if cardsFlipped is less than or equal to 2 to keep player from flipping over more than 2 cards at a time.
     if (state.flippedCards <= 2) {
         card.classList.add('flipped')
     }
@@ -118,14 +136,16 @@ const flipCard = card => {
         }
 
         setTimeout(() => {
-            flipBackCards()
+            backCardFlip()
         }, 1000)
     }
 
     // If there are no more cards that we can flip, we won the game
     if (!document.querySelectorAll('.card:not(.flipped)').length) {
         setTimeout(() => {
-            selectors.boardContainer.classList.add('flipped')
+            // checks if all cards have been flipped; no more cards to be flipped means game is complete
+            selectors.boardContainer.classList.add('flipped') // check if .flipped cards are a match/no match
+            // check the emoji inside card against emoji inside second card; if they match class them by adding .matched
             selectors.win.innerHTML = `
                 <span class="win-text">
                     You won!<br />
@@ -139,13 +159,14 @@ const flipCard = card => {
     }
 }
 
+//Adding Event Listeners directly on the cards
 const attachEventListeners = () => {
     document.addEventListener('click', event => {
         const eventTarget = event.target
         const eventParent = eventTarget.parentElement
 
         if (eventTarget.className.includes('card') && !eventParent.className.includes('flipped')) {
-            flipCard(eventParent)
+            cardFlip(eventParent)
         } else if (eventTarget.nodeName === 'BUTTON' && !eventTarget.className.includes('disabled')) {
             startGame()
         }
@@ -156,7 +177,118 @@ generateGame()
 attachEventListeners()
 
 
-
+const rand = function(min, max) {
+    return Math.random() * ( max - min ) + min;
+  }
+  
+  let canvas = document.getElementById('canvas');
+  let ctx = canvas.getContext('2d');
+  
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  
+  window.addEventListener('resize', function() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx = canvas.getContext('2d');
+    ctx.globalCompositeOperation = 'lighter';
+  });
+  let backgroundColors = [ '#000', '#000' ];
+  let colors = [
+    [ '#002aff', "#009ff2" ],
+    [ '#0054ff', '#27e49b' ], 
+    [ '#202bc5' ,'#873dcc' ]
+  ];
+  let count = 70;
+  let blur = [ 12, 70 ];
+  let radius = [ 1, 120 ];
+  
+  ctx.clearRect( 0, 0, canvas.width, canvas.height );
+  ctx.globalCompositeOperation = 'lighter';
+  
+  let grd = ctx.createLinearGradient(0, canvas.height, canvas.width, 0);
+  grd.addColorStop(0, backgroundColors[0]);
+  grd.addColorStop(1, backgroundColors[1]);
+  ctx.fillStyle = grd;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  
+  let items = [];
+  
+  while(count--) {
+      let thisRadius = rand( radius[0], radius[1] );
+      let thisBlur = rand( blur[0], blur[1] );
+      let x = rand( -100, canvas.width + 100 );
+      let y = rand( -100, canvas.height + 100 );
+      let colorIndex = Math.floor(rand(0, 299) / 100);
+      let colorOne = colors[colorIndex][0];
+      let colorTwo = colors[colorIndex][1];
+      
+      ctx.beginPath();
+      ctx.filter = `blur(${thisBlur}px)`;
+      let grd = ctx.createLinearGradient(x - thisRadius / 2, y - thisRadius / 2, x + thisRadius, y + thisRadius);
+    
+      grd.addColorStop(0, colorOne);
+      grd.addColorStop(1, colorTwo);
+      ctx.fillStyle = grd;
+      ctx.fill();
+      ctx.arc( x, y, thisRadius, 0, Math.PI * 2 );
+      ctx.closePath();
+      
+      let directionX = Math.round(rand(-99, 99) / 100);
+      let directionY = Math.round(rand(-99, 99) / 100);
+    
+      items.push({
+        x: x,
+        y: y,
+        blur: thisBlur,
+        radius: thisRadius,
+        initialXDirection: directionX,
+        initialYDirection: directionY,
+        initialBlurDirection: directionX,
+        colorOne: colorOne,
+        colorTwo: colorTwo,
+        gradient: [ x - thisRadius / 2, y - thisRadius / 2, x + thisRadius, y + thisRadius ],
+      });
+  }
+  
+  
+  function changeCanvas(timestamp) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    let adjX = 2;
+    let adjY = 2;
+    let adjBlur = 1;
+    items.forEach(function(item) {
+      
+        if(item.x + (item.initialXDirection * adjX) >= canvas.width && item.initialXDirection !== 0 || item.x + (item.initialXDirection * adjX) <= 0 && item.initialXDirection !== 0) {
+          item.initialXDirection = item.initialXDirection * -1;
+        }
+        if(item.y + (item.initialYDirection * adjY) >= canvas.height && item.initialYDirection !== 0 || item.y + (item.initialYDirection * adjY) <= 0 && item.initialYDirection !== 0) {
+          item.initialYDirection = item.initialYDirection * -1;
+        }
+        
+        if(item.blur + (item.initialBlurDirection * adjBlur) >= radius[1] && item.initialBlurDirection !== 0 || item.blur + (item.initialBlurDirection * adjBlur) <= radius[0] && item.initialBlurDirection !== 0) {
+          item.initialBlurDirection *= -1;
+        }
+      
+        item.x += (item.initialXDirection * adjX);
+        item.y += (item.initialYDirection * adjY);
+        item.blur += (item.initialBlurDirection * adjBlur);
+        ctx.beginPath();
+        ctx.filter = `blur(${item.blur}px)`;
+        let grd = ctx.createLinearGradient(item.gradient[0], item.gradient[1], item.gradient[2], item.gradient[3]);
+        grd.addColorStop(0, item.colorOne);
+        grd.addColorStop(1, item.colorTwo);
+        ctx.fillStyle = grd;
+        ctx.arc( item.x, item.y, item.radius, 0, Math.PI * 2 );
+        ctx.fill();
+        ctx.closePath();
+      
+    });
+    window.requestAnimationFrame(changeCanvas);
+    
+  }
+  
+  window.requestAnimationFrame(changeCanvas);
 
 
 
@@ -164,7 +296,7 @@ attachEventListeners()
 let matched = 0;
 let cardOne, cardTwo;
 let disableDeck = false;
-function flipCard({target: clickedCard}) {
+function cardFlip({target: clickedCard}) {
     if(cardOne !== clickedCard && !disableDeck) {
         clickedCard.classList.add("flip");
         if(!cardOne) {
@@ -185,8 +317,8 @@ function matchCards(img1, img2) {
                 return shuffleCard();
             }, 1000);
         }
-        cardOne.removeEventListener("click", flipCard);
-        cardTwo.removeEventListener("click", flipCard);
+        cardOne.removeEventListener("click", cardFlip);
+        cardTwo.removeEventListener("click", cardFlip);
         cardOne = cardTwo = "";
         return disableDeck = false;
     }
@@ -211,11 +343,11 @@ function shuffleCard() {
         card.classList.remove("flip");
         let imgTag = card.querySelector(".back-view img");
         imgTag.src = `images/img-${arr[i]}.png`;
-        card.addEventListener("click", flipCard);
+        card.addEventListener("click", cardFlip);
     });
 }
 shuffleCard();
     
 cards.forEach(card => {
-    card.addEventListener("click", flipCard);
+    card.addEventListener("click", cardFlip);
 });*/
